209b1f47ac2651190aebb83b35cb4ec565c8d121
Johnny Eric Amancio <johnnyeric@gmail.com>
2018-06-12 13:38:14 +0100

    fix: Fix sync issues

--- public/index.js
+++ public/index.js
@@ -8,142 +8,154 @@ const _gravity = 0.1;
 const _playerList = {};
 
 class Player {
-  constructor(id, x, y) {
-    /*this.x = 150;
+    constructor(id, x, y, control = false) {
+        /*this.x = 150;
     this.y = 400 / 2;*/
-    this.id = id;
-    this.x = x;
-    this.y = y;
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.control = control;
 
-    this.velocity = 0;
-    this.lift = -5;
+        this.velocity = 0;
+        this.lift = -5;
 
-    this.px = 0;
-    this.py = 0;
-    this.targetX = 0;
-    this.targetY = 0;
+        this.px = 0;
+        this.py = 0;
+        this.targetX = 0;
+        this.targetY = 0;
 
-    this.firedBullets = 0;
+        this.firedBullets = 0;
 
-    this.enableBindings();
-  }
+        this.enableBindings();
+    }
 
-  enableBindings() {
-    document.addEventListener('keyup', (event) => {
-       switch (event.keyCode) {
-          case 32:
-            return this.jump();
-          default:
-             break;
+    enableBindings() {
+        document.addEventListener('keyup', event => {
+            switch (event.keyCode) {
+                case 32:
+                    return this.jump();
+                default:
+                    break;
+            }
+        });
+
+        document.addEventListener('mousemove', event => {
+            this.targetX = event.pageX;
+            this.targetY = event.pageY;
+        });
+
+        document.addEventListener('click', () => {
+            this.fire();
+        });
+    }
+
+    update() {
+        this.velocity += _gravity;
+        this.y += this.velocity;
+
+        if (this.y > 300) {
+            this.velocity -= 1;
         }
-    });
 
-    document.addEventListener('mousemove', (event) => {
-      this.targetX = event.pageX;
-      this.targetY = event.pageY;
-    });
-
-    document.addEventListener('click', () => {
-      this.fire();
-    });
-  }
-
-  update() {
-    this.velocity += _gravity;
-    this.y += this.velocity;
-
-    if (this.y > 300) {
-      this.velocity -= 1;
+        this.render();
     }
 
-    this.render();
-  }
+    render() {
+        ctx.beginPath();
+        ctx.fillStyle = '#ffffff';
+        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+        ctx.moveTo(150, 150);
+        ctx.fill();
 
-  render() {
-    ctx.beginPath();
-    ctx.fillStyle = '#ffffff';
-    ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
-    ctx.moveTo(150,150);
-    ctx.fill();
+        ctx.strokeStyle = '#FF0000';
 
-    ctx.strokeStyle="#FF0000";
+        /**
+         * Rethink mouse movement algorithm
+         */
+        var tx = this.targetX - this.x,
+            ty = this.targetY - this.y,
+            dist = Math.sqrt(tx * tx + ty * ty);
 
-    /**
-     * Rethink mouse movement algorithm
-     */
-    var tx = this.targetX - this.x,
-        ty = this.targetY - this.y,
-        dist = Math.sqrt(tx * tx + ty * ty);
+        var radians = Math.atan2(-ty, -tx);
 
-      var radians = Math.atan2(-ty,-tx);
+        this.px = this.x + 50 * Math.cos(radians);
+        this.py = this.y + 50 * Math.sin(radians);
 
-      this.px = this.x + 50 * Math.cos(radians);
-      this.py = this.y +  50 * Math.sin(radians);
-
-      ctx.strokeStyle = "red";
-      ctx.beginPath();
-      ctx.moveTo(this.x, this.y);
-      ctx.lineTo(this.px, this.py);
-      ctx.closePath();
-      ctx.stroke();
-    /**
-     * End of it
-     */
-  }
-
-  jump() {
-    if (socket.id == this.id) {
-      this.velocity += this.lift;
+        ctx.strokeStyle = 'red';
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.px, this.py);
+        ctx.closePath();
+        ctx.stroke();
+        /**
+         * End of it
+         */
     }
-  }
 
-  fire() {
-    this.firedBullets += 1;
-  }
+    jump() {
+        if (this.control) {
+            this.velocity += this.lift;
+        }
+    }
+
+    fire() {
+        this.firedBullets += 1;
+    }
 }
 
 function gameLoop() {
-  ctx.clearRect(0, 0, 400, 400);
+    ctx.clearRect(0, 0, 400, 400);
 
-  Object.keys(_playerList).map((player) => {
-    _playerList[player].update();
-  });
+    Object.keys(_playerList).map(player => {
+        _playerList[player].update();
+    });
 
-  ctx.rect(30, 350, 150, 10);
-  ctx.fill();
+    ctx.rect(30, 350, 150, 10);
+    ctx.fill();
 
-  requestAnimationFrame(gameLoop);
+    requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
 
 const objectToMap = obj => {
-  const map = new Map();
+    const map = new Map();
 
-  for (const k of Object.keys(obj)) {
-      map.set(k, obj[k]);
-  }
-  return map;
+    for (const k of Object.keys(obj)) {
+        map.set(k, obj[k]);
+    }
+    return map;
 };
 
 socket.on('connect', () => {
-  socket.emit('player_joined', {
-      name: Math.round(Math.random() * 100),
-  });
-
-  socket.on('player_joined', () => {
-    socket.emit('get_players_position');
-  })
-
-  socket.on('players_position', (data) => {
-    const map = objectToMap(data);
-
-    map.forEach((data) => {
-      _playerList[data.id] = new Player(data.id, data.position.x, data.position.y);
+    socket.emit('player_joined', {
+        name: Math.round(Math.random() * 100)
     });
 
-  });
+    socket.on('player_joined', data => {
+        _playerList[data.id] = new Player(
+            data.id,
+            data.position.x,
+            data.position.y,
+            true
+        );
+        socket.emit('get_players_position');
+    });
 
+    socket.on('players_position', data => {
+        const map = objectToMap(data);
 
-  socket.emit('players_position', {});
+        map.forEach(player => {
+            if (player.id === socket.it) {
+                return;
+            }
+            _playerList[player.id] = new Player(
+                player.id,
+                player.position.x,
+                player.position.y
+            );
+        });
+    });
+
+    //socket.emit('players_position', {});
 });
